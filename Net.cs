25c363c0926e3418bd7288adb95eebfd1d6fef10
Andrew <supracharger@yahoo.com>
2025-08-10 18:38:42 -0600

    Added optimization flags for release build in project files.

--- Net.cs
+++ Net.cs
@@ -46,7 +46,7 @@ namespace MiniNet
         // Again, You do not need to define the forward pass & backwards pass explicitly
         public Tensor<float[][]> Foward(Tensor<float[][]> x)
         {
-            // Split into 2 one dim1
+            // Split into 2 on dim1
             var split = Function.Split.Fwd(x, 1, 12, 12);
             x = split[0];
             var xix = split[1];
